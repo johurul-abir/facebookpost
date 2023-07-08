70fed2bbe970b9e0f11edb32d7c42d6b5d3a278f
johurul islam <89250009+johurul-abir@users.noreply.github.com>
2023-07-08 08:35:58 +0600

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,156 @@
+
+const post =[
+    {
+        id : 1,
+        content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit optio necessitatibus id nemo iste quod?",
+        media: "https://embedsocial.com/wp-content/uploads/2020/10/add-links-instagram-posts.jpg",
+        like: 500,
+        comments: 90,
+        timesAgo: "20 min",
+        author:{
+            id:1,
+            name:"Johurul islam",
+            profilePic:"./assets/images/johurul profile pic.png"
+        }
+    },
+
+    {
+        id : 2,
+        content: "❗INFORMATION FROM MICROSOFFT ChatGPT4-V4 is coming soon, exclusively for marketers and advertisers, more professional, 100 times smarter.Use now: https://www.chatgptv4.store/These are the 2 most prominent and favorite features💯Write advertising contentGPT4 Marketing helps users to write advertising, promotion, discount..extremely attractive and attractive content to customers. (Attracting CUSTOMERS INTERESTED IN PRODUCTS AND SERVICES)💯Target the exact object GPT4 Marketing helps advertisers identify relevant keywords to target for their campaigns. This feature will help advertising campaigns become much more diverse and accurate than before. (DOWN ADVERTISING COST, IMPROVE EFFICIENCY many times)PLEASE COMMENT SO THAT THE OFFICIAL Version coming out will be more complete.",
+
+        media: "https://scontent.fdac138-2.fna.fbcdn.net/v/t39.30808-6/356997186_766889855224555_1847730299119645698_n.jpg?stp=dst-jpg_p180x540&_nc_cat=1&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFrzanZS3a8Tdm5Z8yLBllkzir9pitIG2zOKv2mK0gbbK2qjmKR66Q4eHvwurcEUtCnUYDQTx0Nr3UEu2jKOv7c&_nc_ohc=1HbCyC5WQhgAX9_qyBk&_nc_ht=scontent.fdac138-2.fna&oh=00_AfDDbAsHvoZnMqRj2Ye7f7fLZIuFzeUl8hbEsR7SQ60C1Q&oe=64AB82C3",
+        like:2.7 + "K",
+        comments: 110,
+        timesAgo: "Sponsored",
+        author:{
+            id:1,
+            name:"Smartgpt4.com",
+            profilePic:"https://scontent.fdac138-2.fna.fbcdn.net/v/t39.30808-6/355655588_763141262266081_4120397774945039943_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEtS_Hwsy4VK-fuf3jwoHHNXkYHvyUvUVNeRge_JS9RU-spUHiamQRLXTYmwkcbpI0wwoCWvgjjdy7IC5V9ki8V&_nc_ohc=0PLsSYJ1tokAX_gyZD9&_nc_oc=AQkvZfXuyCa602mMWfNhK5c1jTXSNEAzyEIXx4StiVFOs8Kd7XQ-lC0oSduSJrkaoR8&_nc_ht=scontent.fdac138-2.fna&oh=00_AfD3egQ41Ewc90xUPxTWPNb0upoRSWxtxYmHa_k4m91jWg&oe=64AB93BB"
+        }
+    },
+    
+    {
+        id : 3,
+        content: "খুব সুন্দর একটি বাড়ি 🥰",
+        media: "https://scontent.fdac138-2.fna.fbcdn.net/v/t39.30808-6/357700543_6276128762481503_1073568279985797968_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeERVkkGMP_3weGjQ2jh0l4BuyGm94q6fNO7Iab3irp80xPy3gwBCQZSEIVHQJrX9T26mQA1OH5b2YvgIUQtT_-i&_nc_ohc=-KTh9A0TcqQAX94GJ-x&_nc_ht=scontent.fdac138-2.fna&oh=00_AfAniMEC1lzVfW0n8Fy5wekBKx-ut1IjiMQCQjmsbKICNw&oe=64ABA9D0",
+        like: 427 + "K",
+        comments: 3.9 + "K",
+        timesAgo: "17 Hours ago",
+        author:{
+            id:1,
+            name:"Mazbah Uddin",
+            profilePic:"https://scontent.fdac138-2.fna.fbcdn.net/v/t39.30808-6/344567214_770861817704494_3523191547666051961_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEJd5wIiZ3T4xlMB1mgErzztQSeDNlTAE21BJ4M2VMATdXzCCeXw_GYC2z_o2P6ZG3vs8MhR5sIQtWoCM0hiua0&_nc_ohc=9hgAbhl5ZoQAX-HNRxv&_nc_ht=scontent.fdac138-2.fna&oh=00_AfCTKaOlwfZruP2xhKhqKjdXS7ZLnuNxYJnQl8j-lr0mAw&oe=64AAB248"
+        }
+    },
+
+    {
+        id : 4,
+        content: "King Of Pairs 👍",
+
+        media: "https://scontent.fdac138-1.fna.fbcdn.net/v/t39.30808-6/358414829_229490050008467_1367501773525699663_n.jpg?stp=dst-jpg_s640x640&_nc_cat=107&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeHod5WDQ-h_qRV2O3NrmLZ-julNBXD39AWO6U0FcPf0BVpurA9wSH9i2vthP8mHAMxXtNK2DaMNRS5Cne71Y-CM&_nc_ohc=JynB-1aW4uIAX_T0zRd&_nc_ht=scontent.fdac138-1.fna&oh=00_AfAgbtXpXc6Gs05D2531fgxvqT4tyApOdDi6RJfaB1uavg&oe=64AA6F4C",
+        like: 6.2 + "K",
+        comments: 59,
+        timesAgo: "13 hours ago",
+        author:{
+            id:1,
+            name:"King Mbappe Lovers",
+            profilePic:"https://scontent.fdac138-2.fna.fbcdn.net/v/t39.30808-6/329851272_1844587062585187_8106934426707493847_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHU3aMyySJMV_tnYTb5qMgHfvrGrKZGljV--saspkaWNTAj_OiXZSqqLddZIpgchUWdx2AOA1TPy3DzoCqihLh8&_nc_ohc=k-roZOOTd8IAX--Au6-&_nc_ht=scontent.fdac138-2.fna&oh=00_AfASuZTTGjTSScO9qNAlMImIQPVzI7QuXXzhQn4GNRWTjw&oe=64AB26C3"
+        }
+    },
+ 
+    {
+        id : 5,
+        content: "বিলাসী চা!",
+
+        media: "https://scontent.fdac138-2.fna.fbcdn.net/v/t39.30808-6/356709476_814747003486872_2951038080889601640_n.jpg?stp=dst-jpg_p526x296&_nc_cat=1&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFtk9fuJSZW_d1HYDPqdiiAKRfqDEaxVhQpF-oMRrFWFJPvakWSaWMx0_tZY-caKp3vMFcndyVSe9xeGkKgtCaV&_nc_ohc=7m5i8B-kHPAAX-Ve5Va&_nc_ht=scontent.fdac138-2.fna&oh=00_AfACdjNtesdLxMilp8A46OJP3UqQOSS8kiaCleOhhw1x3Q&oe=64AB453D",
+        like: 127 + "K",
+        comments: 1.3 + "K",
+        timesAgo: "1 hour ago",
+        author:{
+            id:5,
+            name:"Sirajganj Express।। সিরাজগঞ্জ এক্সপ্রেস।",
+            profilePic:"https://scontent.fdac138-2.fna.fbcdn.net/v/t39.30808-6/272233946_431064295188480_9050189995929401233_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFM3YVJhIqeETIHLqifxb8sU46_pujNlEBTjr-m6M2UQPRyX0IuZsl2Hl1Br2SayvTwP4ujGpxcBX9zmwJ1dYs1&_nc_ohc=_WLe9gSqIbIAX-Ed3lz&_nc_ht=scontent.fdac138-2.fna&oh=00_AfCNawBFgu1iFv3SNN7fwRa3IqYFHwGqc8JhRwRZWVH0dw&oe=64AAE3FF"
+        }
+    },
+
+    {
+        id : 6,
+        content: "𝐆𝐨𝐨𝐠𝐥𝐞 𝐁𝐚𝐫𝐝 𝐕𝟐 new version 50 times smarter not only supports the advertising and marketing industry, but also knows how to compose music, compose stories, draw pictures, edit videos, design... according to the content you require.Experience here => https://drive.google.com/uc...Installation code : 888AI is fast becoming the future of advertising. Advertisers are already using AI to identify and segment audiences, create ad creatives, test ads, improve ad performance, and optimize spend—all automatically, according real-time, on a massive scale.After using, please take 1 minute to answer our survey questions to make the upcoming official version more complete. Thank for all.",
+
+        media: "https://scontent.fcgp1-1.fna.fbcdn.net/v/t39.30808-6/348290581_649391520568705_4263368025703399397_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeED0wKTUOtaX6eMXlB1Ctcn3I0C2QW5xzTcjQLZBbnHNKPfdJXl_f6YmjB7yMWl5o60rVb-wEqrLFr-4xgRjn-_&_nc_ohc=hWWbP8ltZMYAX9nP5NT&_nc_zt=23&_nc_ht=scontent.fcgp1-1.fna&oh=00_AfA5DYdXBJNckPZ1qAFFQzL9Ybk6U8XkH5A-brnEeVvPJw&oe=64A6BC14",
+        like:324 + "K" ,
+        comments:24 + "K",
+        timesAgo: "1 day ago",
+        author:{
+            id:6,
+            name:"Bard Ai For Business",
+            profilePic:"https://scontent.fcgp1-1.fna.fbcdn.net/v/t39.30808-6/348290581_649391520568705_4263368025703399397_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeED0wKTUOtaX6eMXlB1Ctcn3I0C2QW5xzTcjQLZBbnHNKPfdJXl_f6YmjB7yMWl5o60rVb-wEqrLFr-4xgRjn-_&_nc_ohc=hWWbP8ltZMYAX9nP5NT&_nc_zt=23&_nc_ht=scontent.fcgp1-1.fna&oh=00_AfA5DYdXBJNckPZ1qAFFQzL9Ybk6U8XkH5A-brnEeVvPJw&oe=64A6BC14"
+        }
+    },
+
+    {
+        id : 7,
+        content: "গরীব মানুষের জন্য যার মনে একটু হলেও দয়া হয় 😍সেই মানুষ টার নাম মাশরাফি বিন মুর্তজা ভাই❤️❤️🥰",
+
+        media: "https://scontent.fcgp1-1.fna.fbcdn.net/v/t39.30808-6/356877715_298892999200753_6520697426926024803_n.jpg?stp=dst-jpg_p526x296&_nc_cat=1&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeGjKKVhiB1BIxew3bmmCfvxMcnhyyxEmuQxyeHLLESa5P_3Z8HGJZIwWCGvJzilI0EJvK38TxzoQXJxMZI4zx9R&_nc_ohc=X8oNR17Ln_0AX-h5SNe&_nc_zt=23&_nc_ht=scontent.fcgp1-1.fna&oh=00_AfAIJZYHpdHb2joKWUf8O3psgtEHw9g0lEol0mnYQlHtrQ&oe=64A69258",
+        like:34 + "K",
+        comments:126,
+        timesAgo: "1 day ago",
+        author:{
+            id:6,
+            name:"Azim Uddin",
+            profilePic:"https://scontent.fdac138-2.fna.fbcdn.net/v/t39.30808-6/323863015_1577570396041094_7867049263329041874_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFDZ2X7iMKekHXelb0NA4qKPGHP1d_1Tas8Yc_V3_VNq8gA6WXQ1iOb5LgK0jqMbWpMEJyK1NeZfKiLBajxl9cv&_nc_ohc=HdH5ei3FcAIAX_MaIbU&_nc_ht=scontent.fdac138-2.fna&oh=00_AfCBMxNa2y7A-6aMy3cGj4v_PmlwWWN_PcGf0AHICG1uhQ&oe=64ABD2A3"
+        }
+    },
+
+    {
+        id : 8,
+        content: "Celebrity",
+
+        media: "https://scontent.fdac138-2.fna.fbcdn.net/v/t39.30808-6/357372408_180375045016028_3983836992171208857_n.jpg?stp=dst-jpg_p526x296&_nc_cat=1&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeGD2gptiaxBANFYOPdirOiTYSYf0TE7FS5hJh_RMTsVLm73WBo1BixWsRg3I1x3nub5QjnESmYEVZE2atLb5QFZ&_nc_ohc=wCDb5NqKkrUAX-ACe9L&_nc_ht=scontent.fdac138-2.fna&oh=00_AfDfLH4GSzo8SA2_w6ICq5OXQJfSYGIWisyMraVAR_RgFA&oe=64AB9A18",
+        like:17 + "K",
+        comments:255,
+        timesAgo: "3 days ago",
+        author:{
+            id:7,
+            name:"Celebrity Hub",
+            profilePic:"https://scontent.fdac138-2.fna.fbcdn.net/v/t39.30808-6/337849796_714856677097728_1825560123968983437_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFriVCu3yQ9o_0FbS393oIzItFuQ45nAyoi0W5DjmcDKpJSG9BzLF24-Rymf6eYQXmExY0CY0W_ksmZ6liaZ5Pr&_nc_ohc=h-ngTs_HcNQAX88H22g&_nc_ht=scontent.fdac138-2.fna&oh=00_AfBY7aRih-3hcc7UKLKxbCzcUumIT0EQ5bvuBfaFbMqWIg&oe=64AC1E12"
+        }
+    },
+
+    {
+        id : 9,
+        content: "অবসরের ঘোষণায় যা বললেন তামিম",
+
+        media: "https://scontent.fdac138-2.fna.fbcdn.net/v/t39.30808-6/358583297_661704059329313_4091551923666074116_n.jpg?stp=dst-jpg_p526x296&_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGKpOfSmTRJ7HXx-DjAj_wrTfegx-tiE7RN96DH62ITtGtybuI_UbMz39X0KalDjm3YDpv2F9eQN7PxX2dvgBvm&_nc_ohc=VsxYZ6lFjyoAX_axUSZ&_nc_ht=scontent.fdac138-2.fna&oh=00_AfCIICM0L6QAFwaRorALE54wQHcUZNEi3W-tQK65WONRcw&oe=64ABFCFD",
+        like:81,
+        comments:9,
+        timesAgo: " 81",
+        author:{
+            id:9,
+            name:"Rtv । আরটিভি",
+            profilePic:"https://scontent.fdac138-2.fna.fbcdn.net/v/t39.30808-6/346626565_1709129362893897_1110048420056606852_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGlklIMrKhk8ipDeq85Bcnq00iCh2OPaOjTSIKHY49o6B3mhlBABgyIS-zO_Rdf4tlv2q3ILNx-B3jfqiNxixQi&_nc_ohc=SZVmiW66XvUAX9EzsZ4&_nc_ht=scontent.fdac138-2.fna&oh=00_AfDuuNW5YpGUMKI1IPYoQusvVT5itRXVLqkP1XsfAyIPlQ&oe=64AC502E"
+        }
+    },
+
+    {
+        id : 10,
+        content: "💝 সম্মানিত হাজীগণের সেবাদান কালে।",
+
+        media: "https://scontent.fdac138-1.fna.fbcdn.net/v/t39.30808-6/357522883_650312430449801_3980876675945521122_n.jpg?stp=dst-jpg_p180x540&_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHuU49Ncbe4oOpuZpfNght09RC5hBwRLob1ELmEHBEuhsKAxDlxZSKZXSZroQOBCtIMaaNjy9L85bfEIY1IDdCT&_nc_ohc=EbDhV6xfNc0AX9wbMpc&_nc_ht=scontent.fdac138-1.fna&oh=00_AfCRD1NrGvycjwjp_JpFuE8LMHu-zQ6tEDSk28htDtq_SA&oe=64AC5566",
+        like: 65,
+        comments:19,
+        timesAgo: "3 days ago",
+        author:{
+            id:10,
+            name:"Mohammad Nayem (President's Scout)",
+            profilePic:"https://scontent.fdac138-2.fna.fbcdn.net/v/t39.30808-6/319407797_1538780963290129_1351382500169663089_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEAbau29vG4Z0dvkb3MljmmElQUviPJXmwSVBS-I8lebMeUib_Gm0i2U7TjuPehkErfczZg9NeGVGdfuuK7ax-O&_nc_ohc=9ykR1Fx_EJsAX8bzkV3&_nc_ht=scontent.fdac138-2.fna&oh=00_AfA1V2w51QF-oo4uMkqzhGsPB9O8ZLiJ5mZO4m6vLQsPgw&oe=64AAFC2B"
+        }
+    },
+
+    
+
+
+
+
+]
